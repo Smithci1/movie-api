35a9163b6ed7e78b028d93444a6e99f377ee2d29
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-29 20:57:00 -0400

    post function is done, will now add it to my index and test it out with postman

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(express.json())
  app.get('/movies', getMovies)
  app.get('/movies/:lookup', gettitleorDirector)
+ 
  app.all('*', nono)
  app.listen(2270, () => {
      console.log('i am 2270')
